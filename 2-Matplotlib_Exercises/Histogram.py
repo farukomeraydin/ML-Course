83a7184879242623633ba80565db4c40d4250c7a
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2023-09-03 17:20:02 +0300

    Create Histogram.py

---
+++ 2-Matplotlib_Exercises/Histogram.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+a = [23, 12, 45, 67, 19, 76, 81, 23, 45, 68, 92, 69, 42, 12, 92, 18, 33, 39, 34, 56, 57, 48, 76, 72, 92, 45, 56, 3, 1,
+    88, 82, 87]
+plt.hist(a, bins=20, color='black')
+plt.show()
