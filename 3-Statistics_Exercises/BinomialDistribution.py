32d8292e797ce92fd6856f070100e8f2d7258a43
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2023-09-11 15:38:11 +0300

    Create BinomialDistribution.py

---
+++ 3-Statistics_Exercises/BinomialDistribution.py
@@ -0,0 +1,12 @@
+from scipy.stats import binom
+
+print(binom.pmf(2, 3, 0.5)) #k, n and p values
+
+import math
+
+print(math.comb(3, 2) * 0.5 ** 2 * 0.5) #gives the same value.
+
+from scipy.stats import binom
+
+print(binom.pmf(7, 10, 0.5)) #Probability of 7 sick people from 10.(10 kişiden 7 kişinin hasta olma olasılığı olsun.)
+
