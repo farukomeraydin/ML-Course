d366d495654b3216a5c378c50fca5abe5328d954
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2023-09-11 23:24:23 +0300

    in sklearn module LabelEncoder class is used

--- 4-Preprocessing/CategoricalToNumber-2.py
+++ 4-Preprocessing/CategoricalToNumber-2.py
@@ -4,9 +4,10 @@ df = pd.read_csv("test_categorical.csv", encoding='unicode_escape')
 
 from sklearn.preprocessing import LabelEncoder
 
-le = LabelEncoder() 
-transformed_data = le.fit_transform(df['Cinsiyet'])
-print(transformed_data)
+print(df, end='\n\n')
+le = LabelEncoder()
 
-transformed_data = le.fit_transform(df['Renk Tercihi'])
-print(transformed_data)
+df['Renk Tercihi'] = le.fit_transform(df['Renk Tercihi'])
+df['Cinsiyet'] = le.fit_transform(df['Cinsiyet'])
+
+print(df)
