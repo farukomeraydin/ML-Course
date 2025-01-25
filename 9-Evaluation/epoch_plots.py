228e402ed20995ac468129cdd150977421178c54
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2025-01-25 10:30:38 +0300

    history callback is used

---
+++ 9-Evaluation/epoch_plots.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+df = pd.read_csv('diabetes.csv')
+
+dataset_x = df.iloc[:, :-1].to_numpy()
+dataset_y = df.iloc[:, -1].to_numpy()
+
+from sklearn.model_selection import train_test_split
+
+training_dataset_x, test_dataset_x, training_dataset_y, test_dataset_y = train_test_split(dataset_x, dataset_y)
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+
+model = Sequential(name='Sample')
+model.add(Dense(64, activation='relu', input_dim=dataset_x.shape[1], name='Hidden-1'))
+model.add(Dense(64, activation='relu', name='Hidden-2')) 
+model.add(Dense(1, activation='sigmoid', name='Output'))
+
+model.summary()
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['binary_accuracy'])
+hist = model.fit(training_dataset_x, training_dataset_y, batch_size=32, epochs=200, validation_split=0.2)
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(15, 5)) #genelde düşey eksen daha kısa çizilir.
+plt.title('Epoch-Loss Graph', fontsize=14, fontweight='bold')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.xticks(range(0, 210, 10))
+
+plt.plot(hist.epoch, hist.history['loss'])
+plt.plot(hist.epoch, hist.history['val_loss'])
+plt.legend(['Loss', 'Validation Loss'])
+plt.show()
+
+plt.figure(figsize=(15, 5))
+plt.title('Epoch-Binary Accuracy Graph', fontsize=14, fontweight='bold')
+plt.xlabel('Epochs')
+plt.ylabel('Binary Accuracy')
+plt.xticks(range(0, 210, 10))
+
+plt.plot(hist.epoch, hist.history['binary_accuracy'])
+plt.plot(hist.epoch, hist.history['val_binary_accuracy'])
+plt.legend(['Binary Accuracy', 'Validation Binary Accuracy'])
+plt.show()
