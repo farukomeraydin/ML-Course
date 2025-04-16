c143c58c272fd44e162ee5bedbba2ed2203c4b03
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2025-04-16 18:32:47 +0300

    also now testing it is done

---
+++ 13-Partial_Data_Training/random_samples3.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+EPOCHS = 10
+STEPS_PER_EPOCH = 20
+BATCH_SIZE = 32
+VALIDATION_STEPS = 5
+TEST_STEPS = 10
+
+def data_generator():
+    for i in range(EPOCHS):
+        for k in range(STEPS_PER_EPOCH):
+            x = np.random.random((BATCH_SIZE, 10))
+            y = np.random.randint(0, 2, BATCH_SIZE, dtype='int8')
+            
+            yield x, y
+            
+def validation_generator():
+    for i in range(EPOCHS):
+        for k in range(VALIDATION_STEPS + 1):
+            x = np.random.random((BATCH_SIZE, 10))
+            y = np.random.randint(0, 2, BATCH_SIZE, dtype='int8')
+            yield x, y
+            
+def test_generator():
+    for i in range(TEST_STEPS):
+        x = np.random.random((BATCH_SIZE, 10))
+        y = np.random.randint(0, 2, BATCH_SIZE, dtype='int8')
+        yield x, y
+    
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+
+model = Sequential(name='PartialDataTraining')
+model.add(Dense(64, activation='relu', input_dim=10, name='Hidden-1'))
+model.add(Dense(64, activation='relu', name='Hidden-2'))
+model.add(Dense(1, activation='sigmoid', name='Output'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['binary_accuracy'])
+model.fit(data_generator(), epochs=EPOCHS, steps_per_epoch=STEPS_PER_EPOCH, validation_data=validation_generator(), validation_steps=VALIDATION_STEPS)
+
+eval_result = model.evaluate(test_generator(), steps=TEST_STEPS)
+for i in range(len(eval_result)):
+    print(f'{model.metrics_names[i]}: {eval_result[i]}')
